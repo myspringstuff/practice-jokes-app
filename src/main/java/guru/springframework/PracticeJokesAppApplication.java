e261aa9aa44afd01fcd12e9f87923b9ecc38fc87
Jian-Min Gao <jian-min.gao@hpe.com>
2018-06-25 08:48:37 +0800

    changed to xml configuration

--- src/main/java/guru/springframework/PracticeJokesAppApplication.java
+++ src/main/java/guru/springframework/PracticeJokesAppApplication.java
@@ -2,8 +2,10 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class PracticeJokesAppApplication {
 
 	public static void main(String[] args) {
